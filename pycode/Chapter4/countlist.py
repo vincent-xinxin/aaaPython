52c4c7e729e0c3118ba9a90a3ddd6e5fd982be6a
vin <41158885+vincent-xinxin@users.noreply.github.com>
2019-03-28 15:48:17 +0800

    count list

---
+++ pycode/Chapter4/countlist.py
@@ -0,0 +1,8 @@
+digits = []
+for value in range(0,10):
+    number = value
+    digits.append(number)
+print(digits)
+print('min: ' + str(min(digits)))
+print('max: ' + str(max(digits)))
+print('sum: ' + str(sum(digits)))
--- pycode/Chapter4/numberrange.py
+++ pycode/Chapter4/numberrange.py
@@ -1,2 +1,13 @@
 for value in range(1,5):
     print(value)
+
+print('value step') # 2-11中，每2个取一个值
+for value1 in range(2,11,2):
+    print(value1)
+
+print('squares') # 取1-10的平方，并添加到列表中
+squares = []
+for value2 in range(1,11):
+    square = value2**2
+    squares.append(square)
+print(squares)
