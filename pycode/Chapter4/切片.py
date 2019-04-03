f28c9ccac54b4e8d3790edf4b22d95d195339bfa
vin <41158885+vincent-xinxin@users.noreply.github.com>
2019-04-03 17:49:39 +0800

    pm

--- pycode/Chapter4/切片.py
+++ pycode/Chapter4/切片.py
@@ -4,4 +4,4 @@ print(player[1:4])
 print(player[1:])#位置1之后的所有（包含1）
 print(player[:3])#位置3之前的所有（不包含3）输出0，1，2
 print(player[-3:])#无论list内有多少元素，只取list最后3个
-print(player[:-3])#0到倒数第三个（不包含倒数第三个）KS
+print(player[:-3])#0到倒数第三个（不包含倒数第三个）
---
+++ pycode/Chapter4/遍历切片.py
@@ -0,0 +1,1 @@
+player = ['charles','martina','michael','eli','rose']
