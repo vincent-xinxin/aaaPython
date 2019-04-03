7052d11c575717398887ecbb19aea18643457aee
vin <41158885+vincent-xinxin@users.noreply.github.com>
2019-04-03 11:45:26 +0800

    切片

--- pycode/Chapter4/切片.py
+++ pycode/Chapter4/切片.py
@@ -1,2 +1,7 @@
 player = ['charles','martina','michael','eli','rose']
-print(player[0:3])''
+print(player[0:3])#输出列表0-3位置的名字，而且输出也是一个list
+print(player[1:4])
+print(player[1:])#位置1之后的所有（包含1）
+print(player[:3])#位置3之前的所有（不包含3）输出0，1，2
+print(player[-3:])#无论list内有多少元素，只取list最后3个
+print(player[:-3])#0到倒数第三个（不包含倒数第三个）KS
